bbb5b0cc7d135b88e5f2d094cd5f6e035db8d2ee
William Loftus <wloftus444@gmail.com>
2015-03-24 13:52:51 -0400

    Initial Commit

---
+++ Sneezer/SneezeHost.swift
@@ -0,0 +1,13 @@
+//
+//  SneezeHost.swift
+//  Sneezer
+//
+//  Created by William Loftus on 3/24/15.
+//  Copyright (c) 2015 William Loftus. All rights reserved.
+//
+
+import UIKit
+
+class SneezeHost: NSObject {
+
+}
