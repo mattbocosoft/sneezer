90383e3d37d6130798ff43cdd2555e81dab72b73
Matthew Thomas <matt@bocosoft.net>
2015-05-20 18:18:51 -0400

    [AngelNode] Preload textures and the cloud node

--- Sneezer/AngelNode.swift
+++ Sneezer/AngelNode.swift
@@ -11,29 +11,37 @@ import SpriteKit
 
 class AngelNode: SKSpriteNode {
 
+	private let angelTexture = SKTexture(imageNamed: "Angel.png")
+	private let cloudTexture = SKTexture(imageNamed: "CloudExplosion.png")
+
+	private var cloudNode: SKSpriteNode!
+
 	init() {
 
 		let name = "Angel"
-		let texture = SKTexture(imageNamed: "\(name).png")
-		super.init(texture: texture, color: nil, size: CGSizeMake(100, 100))
+		let textureSize = CGSizeMake(100, 100)
+
+		super.init(texture: angelTexture, color: nil, size: textureSize)
 
 		self.name = name
-
 		self.physicsBody = SKPhysicsBody(circleOfRadius: self.frame.size.width/3)
 		self.physicsBody?.friction = 0.0
 		self.physicsBody?.restitution = 1.0
 		self.physicsBody?.linearDamping = 0.0
+		self.physicsBody?.allowsRotation = false
+
+		SKTexture.preloadTextures([self.angelTexture, self.cloudTexture], withCompletionHandler: { () -> Void in
+		})
+
+		let cloudName = "CloudExplosion"
+		self.cloudNode = SKSpriteNode(texture: self.cloudTexture, size: self.size)
+		self.cloudNode.name = name
+		self.cloudNode.alpha = 0.0
 	}
 
 	func poof() {
 
-		let cloudName = "CloudExplosion"
-		let cloudNode = SKSpriteNode(imageNamed: "\(cloudName).png")
-
-		cloudNode.size = self.size
-		cloudNode.name = name
-		cloudNode.alpha = 0.0
-		self.addChild(cloudNode)
+		self.addChild(self.cloudNode)
 
 		let inDuration = 0.2
 		let outDuration = 1.0
@@ -41,10 +49,10 @@ class AngelNode: SKSpriteNode {
 		let cloudMaximumScale = CGFloat(1.5)
 
 		let inGroup = SKAction.group([SKAction.fadeInWithDuration(inDuration), SKAction.scaleTo(cloudMaximumScale, duration: inDuration)])
-		cloudNode.runAction(inGroup, completion: { () -> Void in
+		self.cloudNode?.runAction(inGroup, completion: { () -> Void in
 
-			cloudNode.removeFromParent()
-			self.texture = SKTexture(imageNamed: "\(cloudName).png")
+			self.cloudNode.removeFromParent()
+			self.texture = self.cloudTexture
 			self.setScale(cloudMaximumScale)
 
 			let fadeOutAction = SKAction.fadeOutWithDuration(outDuration)
